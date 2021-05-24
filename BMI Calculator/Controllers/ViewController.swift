a635eaf300abe74d3ac21ac57e64d5c4d4e19be4
Thomas Strauß <thst@beanfactory.de>
2021-05-24 22:17:29 +0200

    connecting the sliders to labels and model

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    var bmiBrain = BMIBrain(height: 1.9, weight: 80)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    private func updateUI() {
+        heightSlider.value = bmiBrain.height
+        weightSlider.value = Float(bmiBrain.weight)
 
+        let weightLabelValue = String(format: "%dkg", bmiBrain.weight)
+        weightLabel.text = weightLabelValue
+
+        let heightLabelValue = String(format: "%.2fm", bmiBrain.height)
+        heightLabel.text = heightLabelValue
+    }
+
+    @IBAction func onSliderValueChanged(_ sender: UISlider) {
+        if sender.tag == 1 {
+            bmiBrain.weight = Int(sender.value)
+        }
+        else {
+            bmiBrain.height = sender.value
+        }
+
+        updateUI()
+    }
+    
 }
 
